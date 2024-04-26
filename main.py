2ee7ceb25788744b92554ec7a155d6da001a5fca
ruslan2020202 <ruslan020507@gmail.com>
2024-04-26 22:40:34 +0300

    add schedule

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import json
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
+import aioschedule
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 
@@ -26,22 +27,40 @@ class Parser:
     params = {
         "ids": "bitcoin",
         "vs_currencies": "usd",
+        "include_24hr_change": "true"
     }
 
-    def get_coin_price(self):
+    async def get_coin_price(self):
         response = requests.get(self.url, headers=self.headers, params=self.params)
         with open('data.json', 'w') as file:
             data = json.loads(response.text)
             data['bitcoin']['time'] = datetime.datetime.now().strftime("%H:%M")
+            data['bitcoin']['usd_24h_change'] = round(data['bitcoin']['usd_24h_change'], 2)
             file.write(json.dumps(data))
 
     def check_coin_price(self):
         with open('data.json', 'r') as file:
             data = json.loads(file.read())
-            return f"BTC: {data['bitcoin']['usd']} $\n\nLast update: {data['bitcoin']['time']}"
+            change_price = f"{data['bitcoin']['usd_24h_change']}%"
+            if int(data['bitcoin']['usd_24h_change']) > 0:
+                change_price += '💹'
+            else:
+                change_price += "🔻"
+            return (f"BTC: {data['bitcoin']['usd']} $\n\n"
+                    f"Change price with 24 hours: {change_price}\n\n"
+                    f"Last update: {data['bitcoin']['time']}")
 
 
 parser = Parser()
+
+asyncio.run(parser.get_coin_price())
+
+
+async def update_price():
+    while True:
+        await parser.get_coin_price()
+        await asyncio.sleep(60 * 15)
+        print('update price')
 
 
 @dp.message(CommandStart())
@@ -61,10 +80,22 @@ async def cmd_photo(message: Message):
     await message.answer_photo(photo1, caption='Photo')
 
 
+async def main():
+    tasks = [
+        asyncio.create_task(update_price()),
+        asyncio.create_task(dp.start_polling(bot))
+    ]
+    try:
+        await asyncio.gather(*tasks)
+    except KeyboardInterrupt:
+        for task in tasks:
+            task.cancel()
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     try:
-        print('Start Bot')
-        asyncio.run(dp.start_polling(bot))
+        asyncio.run(main())
     except KeyboardInterrupt:
         print('Exit')
